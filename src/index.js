d8d569973a314dc0dd144e824b12d99df3c7b1c4
karthi909 <karthikeyareddy440@gmail.com>
2022-04-11 21:35:01 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
 
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect("mongodb+srv://KA909_1:karthi123@cluster0.qpomb.mongodb.net/KA909_1",{
+    useNewUrlParser: true
+})
+.then(()=> console.log('mongoDb is connected'))
+.catch(err => console.log(err))
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
 
-app.listen(process.env.PORT || 4000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 4000))
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
